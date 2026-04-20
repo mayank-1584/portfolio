93ef81de753f01fe9f324d4fd46ab85f9d3cf68a
mayank-1584 <153091323+mayank-1584@users.noreply.github.com>
2026-04-20 21:18:34 +0530

    Add Trophy and Target icons to PlayerCard

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { Github, Linkedin, Instagram, Twitter, Dribbble, MessageCircle } from "lucide-react";
+import { Github, Linkedin, Instagram, Twitter, Dribbble, MessageCircle,Trophy,Target } from "lucide-react";
 import avatar from "@/assets/avatar.jpg";
 
 export function PlayerCard() {
@@ -61,7 +61,7 @@ export function PlayerCard() {
           {/* Social pills + CTA */}
           <div className="flex items-center justify-between gap-2">
             <div className="flex gap-1.5">
-              {[{Icon: Github, href: "https://github.com/mayank-1584"}, {Icon: Linkedin, href: "https://www.linkedin.com/in/mayank-sharma-ab539b283/"}, {Icon: Twitter, href: "https://leetcode.com/u/tGwSk8GHrt/"}, {Icon: Instagram, href: "https://www.instagram.com/mayank_1584/"}, {Icon: Dribbble, href: "https://codeforces.com/profile/mksharmakaku1584"}].map(({Icon, href}, i) => (
+              {[{Icon: Github, href: "https://github.com/mayank-1584"}, {Icon: Linkedin, href: "https://www.linkedin.com/in/mayank-sharma-ab539b283/"}, {Icon: Target, href: "https://leetcode.com/u/tGwSk8GHrt/"}, {Icon: Instagram, href: "https://www.instagram.com/mayank_1584/"}, {Icon: Trophy, href: "https://codeforces.com/profile/mksharmakaku1584"}].map(({Icon, href}, i) => (
                 <a
                   key={i}
                   href={href}
